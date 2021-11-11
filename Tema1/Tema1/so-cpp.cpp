941153fe809eef0e98a4200f131ad41d3f282e0d
ccflorinel99 <paulflorinel1234@gmail.com>
2021-11-11 20:00:06 +0200

    Update 11.11.2021 v 2.0

--- Tema1/Tema1/so-cpp.cpp
+++ Tema1/Tema1/so-cpp.cpp
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <string>
 
-#define macro_no_value ""
+#define no_value ""
 
 
 int check_space(char* sir)
@@ -74,7 +75,7 @@ char* get_macro_name_without_space_before(char* sir)
 
 char* get_value(char* sir)
 {
-	const char s[] = "=";
+	const char s[] = "[=]";
 	char* token;
 	int counter = 0;
 	/* get the first token */
@@ -91,9 +92,9 @@ char* get_value(char* sir)
 
 	if (counter == 1) // daca nu am valoare
 	{
-		char* no_value = (char*)malloc(strlen(macro_no_value) * sizeof(char));
-		strcpy(no_value, macro_no_value);
-		return no_value;
+		char* str_no_value = (char*)malloc(strlen(no_value) * sizeof(char));
+		strcpy(str_no_value, no_value);
+		return str_no_value;
 	}
 }
 
@@ -110,46 +111,262 @@ char* get_directory_name(char* sir) // pt cazul in care nu am spatiu intre -Inum
 
 }
 
-void testare()
+int check_infile(char* sir)
 {
-	char ex7[] = "-Inume_dir";
-
-	char* nume_var7 = get_directory_name(ex7);
-	fprintf(stdout, "Nume director: %s\n", nume_var7);
-
-
+	if (strcmp(sir, "-o") != 0) return 1;
+	else return 0;
 }
 
-//voi afisa fisierul preprocesat la stdout
-/* structura: so-cpp [-D <SYMBOL>[=<MAPPING>]] [-I <DIR>] [<INFILE>] [ [-o] <OUTFILE>] */
-/* so-cpp -D macro[=valoare] -I dir infile -o outfile*/
-int main(int argc, char* argv[])
+int check_o_option(char* sir)
 {
-	testare();
-	char* denumire_macro = NULL, * valoare_macro = NULL, * directory_name = NULL;
-	/* argc = 7 maxim
-	   argc = 4 minim*/
-	if (argc == 1) fprintf(stdout, "No arguments. Exiting....");
+	if (check_infile(sir) == 0) // folosesc functia infile ca sa verific existensa sirului "-o", astfel nu mai dublez secvente de cod
+		return 1;
+	else return 0;
+}
+
+void find_outfile(char* argv[], int index, int necesita_increm, char* nume_outfile, int total_argumente_pt_argv, const char* message)
+{
+	if(necesita_increm == 1) index++;
+
+	if (check_o_option(argv[index])) // -o e prezent
+	{
+		index++;
+		nume_outfile = argv[index];
+		// -D macro[=valoare] -I dir infile -o outfile
+		strcpy((char*)message, " -o outfile");
+
+		if (total_argumente_pt_argv == index) fprintf(stdout, "Am ajuns in cazul %s.\n", message);
+		else fprintf(stdout, "Ceva e in neregula. Sunt in cazul %s.\n", message);
+	}
 	else
 	{
-		// -D macro[=valoare]
-		if (check_space(argv[2]) == 1) // daca am spatiu intre -D si numele variabilei
+		if (total_argumente_pt_argv == index)
 		{
-			denumire_macro = get_macro_name_with_space_before(argv[3]);
-			valoare_macro = get_value(argv[3]);
-			// -I dir
-			if (check_space(argv[4])) directory_name = argv[5];
-			else directory_name = get_directory_name(argv[4]);
+			nume_outfile = argv[index]; // daca outfile exista
+			strcpy((char*)message, " nume_outfile");
+			fprintf(stdout, "Am ajuns in cazul %s.\n", message);
 		}
 		else
 		{
-			denumire_macro = get_macro_name_without_space_before(argv[2]);
-			valoare_macro = get_value(argv[2]);
-			// -I dir
-			if (check_space(argv[3])) directory_name = argv[4];
-			else directory_name = get_directory_name(argv[3]);
+			strcpy(nume_outfile, no_value); // daca nu exista, semnalez cumva
+			fprintf(stdout, "Am ajuns in cazul %s.\n", message);
+		}
+	}
+}
+
+void find_infile_and_outfile(char* argv[], int index, int continua, char* nume_infile, char* nume_outfile, int total_argumente_pt_argv, const char* message)
+{
+	//if(continua==1) => necesita incremenare
+	index++;
+
+	if (check_infile(argv[index])) // infile exista
+	{
+		nume_infile = argv[index];
+		strcpy((char*)message, " infile");
+		find_outfile(argv, index, continua, nume_outfile, total_argumente_pt_argv, message);
+	}
+	else // infile nu exista, ceea ce inseamna ca input-ul va fi primit de la stdin
+	{
+		strcpy(nume_infile, no_value); // voi citi de la stdin
+
+		continua = 0; // resetare
+
+		if (total_argumente_pt_argv == index) continua = 0;
+		else continua = 1;
+
+		if (continua == 1)
+		{
+			find_outfile(argv, index, continua, nume_outfile, total_argumente_pt_argv, message);
+		}
+		else // daca nu mai am argumente
+		{
+			strcpy(nume_outfile, no_value);
+			fprintf(stdout, "Am ajuns in cazul %s.\n", message);
+		}
+	}
+}
+
+void check_for_other_options(char* argv[], int index, int continua, char* nume_infile, char* nume_outfile, int total_argumente_pt_argv, const char* message)
+{
+	if (total_argumente_pt_argv == index) continua = 0; // verific daca mai e ceva sau restul optiunilor lipsesc
+	else continua = 1;
+
+	if (continua == 1)
+	{
+		find_infile_and_outfile(argv, index, continua, nume_infile, nume_outfile, total_argumente_pt_argv, message);
+	}
+	else
+	{
+		strcpy(nume_infile, no_value); // nu exista infile; va fi citit de la stdin
+		strcpy(nume_outfile, no_value); // nu exista outfile; va fi afisat la consola
+		fprintf(stdout, "Am ajuns in cazul %s.\n", message);
+	}
+
+}
+
+void get_directory(char* argv[], int index, int continua, char* directory_name, char* nume_infile, char* nume_outfile, int total_argumente_pt_argv, const char* message)
+{
+	index++;
+	if (check_space(argv[index])) // -I dir sau -Idir
+	{
+		index++;
+		directory_name = argv[index]; // -I dir
+
+		strcpy((char*)message, " -I dir");
+		check_for_other_options(argv, index, continua, nume_infile, nume_outfile, total_argumente_pt_argv, message);
+	}
+	else // -Idir
+	{
+		directory_name = get_directory_name(argv[index]);
+
+		strcpy((char*)message, " -Idir");
+		check_for_other_options(argv, index, continua, nume_infile, nume_outfile, total_argumente_pt_argv, message);
+	}
+
+}
+
+void get_all_details(char* argv[], int index, int continua, char* denumire_macro, char* valoare_macro, char* directory_name, char* nume_infile, char* nume_outfile, int total_argumente_pt_argv)
+{
+	if (check_space(argv[index]) == 1) // -D macro[=valoare]
+	{
+		index++; // x = 2
+		denumire_macro = get_macro_name_with_space_before(argv[index]);
+		valoare_macro = get_value(argv[index]);
+
+		get_directory(argv, index, continua, directory_name, nume_infile, nume_outfile, total_argumente_pt_argv, "-D macro[=valoare]");
+	}
+	else // -Dmacro[=valoare]
+	{
+		denumire_macro = get_macro_name_with_space_before(argv[index]);
+		valoare_macro = get_value(argv[index]);
+
+		get_directory(argv, index, continua, directory_name, nume_infile, nume_outfile, total_argumente_pt_argv, "-Dmacro[=valoare]");
+	}
+
+}
+
+#define max_size 200
+
+int check_define(char* sir)
+{
+	if (strcmp(sir, "#define") == 0) return 1;
+	else return 0;
+}
+
+int check_include(char* sir)
+{
+	if (strcmp(sir, "#include") == 0) return 1;
+	else return 0;
+}
+
+int check_if(char* sir)
+{
+	if (strcmp(sir, "#if") == 0) return 1;
+	else return 0;
+}
+
+int check_elif(char* sir)
+{
+	if (strcmp(sir, "#elif") == 0) return 1;
+	else return 0;
+}
+
+int check_endif(char* sir)
+{
+	if (strcmp(sir, "#endif") == 0) return 1;
+	else return 0;
+}
+
+int check_ifdef(char* sir)
+{
+	if (strcmp(sir, "#ifdef") == 0) return 1;
+	else return 0;
+}
+
+int check_ifndef(char* sir)
+{
+	if (strcmp(sir, "#ifndef") == 0) return 1;
+	else return 0;
+}
+
+void dezlipire(char* sir)
+{
+	const char s[] = " \t\n";
+	char* token;
+
+	/* get the first token */
+	token = strtok(sir, s);
+
+	/* walk through other tokens */
+	while (token != NULL)
+	{
+		if (check_define(token))
+		{
+			//urmeaza ce e dupa define
+		}
+		else if (check_include(token))
+		{
+			// iau continutul fisierului de dupa #include si il afisez
+		}
+		else if (check_if(token))
+		{
+			// fac ce urmeaza dupa #if
+		}
+		else if (check_elif(token))
+		{
+			// fac ce urmeaza dupa #elif
+		}
+		else if (check_endif(token))
+		{
+			// s-a terminat partea de #if
+		}
+		else if (check_ifdef(token))
+		{
+			// fac ce urmeaza dupa #ifdef
+		}
+		else if (check_ifndef(token))
+		{
+			// fac ce urmeaza pt #ifndef
 		}
 
+
+		token = strtok(NULL, s);
+	}
+}
+
+void executing(char* denumire_macro, char* valoare_macro, char* directory_name, char* nume_infile, char* nume_outfile)
+{
+	int define_found = 0, if_foud = 0, elif_found = 0, else_found = 0, ifdef_found = 0, ifndef_found = 0, endif_found = 0, include_found = 0;
+	char buf[max_size];
+
+	if (strcmp(nume_infile, no_value)) // citesc de la stdin
+	{
+		if (strcmp(nume_outfile, no_value) == 0) // afisez la stdout
+		{
+			fgets(buf, max_size, stdin);
+
+		}
+	}
+}
+
+/* structura: so-cpp [-D <SYMBOL>[=<MAPPING>]] [-I <DIR>] [<INFILE>] [ [-o] <OUTFILE>] */
+/* so-cpp -D macro[=valoare] -I dir infile -o outfile*/
+// testul 6 -> -o nu apare
+int main(int argc, char* argv[])
+{
+	char* denumire_macro = NULL, * valoare_macro = NULL, * directory_name = NULL, * nume_infile = NULL, * nume_outfile = NULL;
+	int x = 1; // ca sa tin evidenta indexului lui argv
+	int total_argumente_pt_argv = argc - 1; // argv[1] - argv[argc-1]
+	int continua = 0;
+	/* argc = 8 maxim
+	   argc = 4 minim*/
+	// argv[0] e numele programului
+
+	if (argc == 0) fprintf(stdout, "No arguments. Exiting....");
+	else
+	{
+		get_all_details(argv, x, continua, denumire_macro, valoare_macro, directory_name, nume_infile, nume_outfile, total_argumente_pt_argv);
 		
+		executing(denumire_macro, valoare_macro, directory_name, nume_infile, nume_outfile);
 	}
 }
