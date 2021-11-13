13e2e0121e5a5d403c018110285d339687651d2c
ccflorinel99 <paulflorinel1234@gmail.com>
2021-11-13 20:54:57 +0200

    Update 13.11.2021

--- Tema1/Tema1/so-cpp.cpp
+++ Tema1/Tema1/so-cpp.cpp
@@ -222,16 +222,72 @@ void get_directory(char* argv[], int index, int continua, char* directory_name, 
 		strcpy((char*)message, " -Idir");
 		check_for_other_options(argv, index, continua, nume_infile, nume_outfile, total_argumente_pt_argv, message);
 	}
-
 }
 
-void get_all_details(char* argv[], int index, int continua, char* denumire_macro, char* valoare_macro, char* directory_name, char* nume_infile, char* nume_outfile, int total_argumente_pt_argv)
+int get_another_macro_or_directory(char* argv[], int index, char** pereche_macro_valoare, char option)
 {
+	if (argv[index][1] == option && option == 'D') // daca am optiunea respectiva
+	{
+		char* noua_pereche_macro_valoare = NULL;
+		char* denumire_macro = NULL;
+		char* valoare_macro = NULL;
+		
+		if (check_space(argv[index]) == 1) // -D macro[=valoare]
+		{
+			index++;
+			denumire_macro = get_macro_name_with_space_before(argv[index]);
+			valoare_macro = get_value(argv[index]);
+			noua_pereche_macro_valoare = (char*)malloc((strlen(denumire_macro) + strlen(valoare_macro) + 2) * sizeof(char));
+			strcpy(noua_pereche_macro_valoare, denumire_macro);
+			strcpy(noua_pereche_macro_valoare, valoare_macro);
+			strcpy(*pereche_macro_valoare, noua_pereche_macro_valoare);
+		}
+		else // -Dmacro[=valoare]
+		{
+			denumire_macro = get_macro_name_with_space_before(argv[index]);
+			valoare_macro = get_value(argv[index]);
+		}
+
+		return 1;
+	}
+	else if (argv[index][1] == option && option == 'I')
+	{
+		index++;
+		char* directory_name = NULL;
+
+		if (check_space(argv[index])) // -I dir sau -Idir
+		{
+			index++;
+			directory_name = argv[index]; // -I dir
+
+			
+		}
+		else // -Idir
+		{
+			directory_name = get_directory_name(argv[index]);
+		}
+
+		return 1;
+	}
+
+	return 0;
+}
+
+void get_all_details(char* argv[], int index, int continua, char** pereche_macro_valoare, char* directory_name, char* nume_infile, char* nume_outfile, int total_argumente_pt_argv)
+{
+	char* noua_pereche_macro_valoare = NULL;
+	char* denumire_macro = NULL;
+	char* valoare_macro = NULL;
+
 	if (check_space(argv[index]) == 1) // -D macro[=valoare]
 	{
-		index++; // x = 2
+		index++;
 		denumire_macro = get_macro_name_with_space_before(argv[index]);
 		valoare_macro = get_value(argv[index]);
+		noua_pereche_macro_valoare = (char*)malloc((strlen(denumire_macro) + strlen(valoare_macro) + 2) * sizeof(char));
+		strcpy(noua_pereche_macro_valoare, denumire_macro);
+		strcpy(noua_pereche_macro_valoare, valoare_macro);
+		strcpy(*pereche_macro_valoare, noua_pereche_macro_valoare);
 
 		get_directory(argv, index, continua, directory_name, nume_infile, nume_outfile, total_argumente_pt_argv, "-D macro[=valoare]");
 	}
@@ -239,10 +295,13 @@ void get_all_details(char* argv[], int index, int continua, char* denumire_macro
 	{
 		denumire_macro = get_macro_name_with_space_before(argv[index]);
 		valoare_macro = get_value(argv[index]);
+		noua_pereche_macro_valoare = (char*)malloc((strlen(denumire_macro) + strlen(valoare_macro) + 2) * sizeof(char));
+		strcpy(noua_pereche_macro_valoare, denumire_macro);
+		strcpy(noua_pereche_macro_valoare, valoare_macro);
+		strcpy(*pereche_macro_valoare, noua_pereche_macro_valoare);
 
 		get_directory(argv, index, continua, directory_name, nume_infile, nume_outfile, total_argumente_pt_argv, "-Dmacro[=valoare]");
 	}
-
 }
 
 #define max_size 200
@@ -295,16 +354,27 @@ void check_if_structure(int any_if_found)
 	else fprintf(stdout, "Structura #if nu e buna\n");
 }
 
-void read_from_file_for_include(char* sir, char* outfile, char* directory_name)
+void scriere_stdout_pt_include(char* sir, char* directory_name, char* buf)
 {
-	// #include <>
-	// #include ""
+	char* filename = (char*)malloc((strlen(directory_name) + strlen(sir) + 2) * sizeof(char));
+	strcpy(filename, directory_name);
+	strcpy(filename, sir);
 
-	const char s[] = "<>""";
-	char* token;
-	char buf[max_size];
+	FILE* f = fopen(filename, "r");
+	fseek(stdout, 0, SEEK_END);
 
-	token = strtok(sir, s);
+	while (!feof(f))
+	{
+		fgets(buf, max_size, f);
+		fputs(buf, stdout);
+	}
+
+	fprintf(stdout, "\n");
+	fclose(f);
+}
+
+void scriere_in_fisier_pt_include(char* sir, char* directory_name, char* buf, char* outfile)
+{
 	char* filename = (char*)malloc((strlen(directory_name) + strlen(sir) + 2) * sizeof(char));
 	strcpy(filename, directory_name);
 	strcpy(filename, sir);
@@ -324,6 +394,20 @@ void read_from_file_for_include(char* sir, char* outfile, char* directory_name)
 	fclose(g);
 }
 
+void read_from_file_for_include(char* sir, char* outfile, char* directory_name)
+{
+	// #include <>
+	// #include ""
+
+	const char s[] = "<>""";
+	char* token;
+	char buf[max_size];
+	token = strtok(sir, s);
+
+	if (strcmp(outfile, no_value) == 0) scriere_stdout_pt_include(token, directory_name, buf);
+	else scriere_in_fisier_pt_include(token, directory_name, buf, outfile);
+}
+
 char* define_found(char* token, char* s)
 {
 	token = strtok(NULL, s);
@@ -338,31 +422,26 @@ char* define_found(char* token, char* s)
 	return pereche_macro_valoare;
 }
 
-int ifdef_found(char* nume_macro_de_verificat, char*** pereche_macro_valoare, int nr_perechi, int nr_macros)
+int ifdef_found(char* nume_macro_de_verificat, char** pereche_macro_valoare, int nr_macros)
 {
 	if (pereche_macro_valoare == NULL || nume_macro_de_verificat == NULL) return 0;
 
-	for (int i = 0; i < nr_perechi; i++)
+	for (int i = 0; i < nr_macros; i+=2)
 	{
-		for (int j = 0; j < nr_macros; j += 2)
-		{
-			if (strcmp(nume_macro_de_verificat, pereche_macro_valoare[i][j]) == 0) return 1;
-		}
+		if (strcmp(nume_macro_de_verificat, pereche_macro_valoare[i]) == 0) return 1;
 	}
 	return 0;
 }
 
-int ifndef_found(char* nume_macro_de_verificat, char*** pereche_macro_valoare, int nr_perechi, int nr_macros)
+int ifndef_found(char* nume_macro_de_verificat, char** pereche_macro_valoare, int nr_macros)
 {
 	if (pereche_macro_valoare == NULL || nume_macro_de_verificat == NULL) return 0;
 
-	for (int i = 0; i < nr_perechi; i++)
+	for (int i = 0; i < nr_macros; i += 2)
 	{
-		for (int j = 0; j < nr_macros; j += 2)
-		{
-			if (strcmp(nume_macro_de_verificat, pereche_macro_valoare[i][j]) == 0) return 0;
-		}
+		if (strcmp(nume_macro_de_verificat, pereche_macro_valoare[i]) == 0) return 0;
 	}
+	
 	return 1;
 }
 
@@ -484,24 +563,10 @@ int if_elif_statement(char* token, char* s)
 	else return 0;
 }
 
-void testare()
+void initializare_elemente_necesare_rularii(char* token, char* s, char* directory_name, char* outfile, char** pereche_macro_valoare, int* nr_perechi, int* nr_total)
 {
-	read_from_file_for_include((char*)"test.txt", (char*)"out.txt", (char*)"C:/Users/User/Desktop/ATM/Anul_3/Semestrul_1/PSO/Teme-PSO/Tema1/Tema1/");
-}
+	int if_def_found, if_ndef_found, if_elif, any_type_if_found = 0;
 
-void dezlipire(char* sir, char* directory_name, char* infile, char* outfile)
-{
-	// posibil sa am directive de define sau include in structurile conditionale, asa ca pt ele resetez flag-urile pt include si define
-	const char s[] = " \t\n";
-	char* token;
-	char** pereche_macro_valoare = NULL;
-	int nr_perechi = 1, nr = nr_perechi * 2, if_def_found, if_ndef_found, if_elif, any_type_if_found = 0;
-
-
-	/* get the first token */
-	token = strtok(sir, s);
-
-	/* walk through other tokens */
 	while (token != NULL)
 	{
 		if (check_define(token))
@@ -511,18 +576,18 @@ void dezlipire(char* sir, char* directory_name, char* infile, char* outfile)
 			token = strtok(NULL, s);
 			char* valoare = token;
 
-			if (nr_perechi >= 1)
+			if (*nr_perechi >= 1)
 			{
-				pereche_macro_valoare = (char**)realloc(pereche_macro_valoare, nr_perechi * sizeof(char*));
-				pereche_macro_valoare[nr_perechi - 1] = (char*)malloc(max_size * sizeof(char));
+				pereche_macro_valoare = (char**)realloc(pereche_macro_valoare, *nr_perechi * sizeof(char*));
+				pereche_macro_valoare[*nr_perechi - 1] = (char*)malloc(max_size * sizeof(char));
 			}
 			/*
 			[0] = nume macro
 			[1] = valoare macro
 			*/
-			pereche_macro_valoare[nr_perechi - 1] = define_found(token, (char*)s);
-			nr_perechi++;
-			nr = nr_perechi * 2;
+			pereche_macro_valoare[*nr_perechi - 1] = define_found(token, (char*)s);
+			*nr_perechi++;
+			*nr_total = *nr_perechi * 2;
 		}
 		else if (check_include(token))
 		{
@@ -547,35 +612,100 @@ void dezlipire(char* sir, char* directory_name, char* infile, char* outfile)
 			any_type_if_found = 1;
 			token = strtok(NULL, s);
 
-			if_def_found = ifdef_found(token, &pereche_macro_valoare, nr_perechi, nr);
+			if_def_found = ifdef_found(token, pereche_macro_valoare, *nr_total);
 		}
 		else if (check_ifndef(token))
 		{
 			any_type_if_found = 1;
 			token = strtok(NULL, s);
 
-			if_ndef_found = ifndef_found(token, &pereche_macro_valoare, nr_perechi, nr);
-		}
-		else
-		{
-			// am trecut de prima bucata (cele cu #), iar acum trebuie sa completez macro-urile cu valoarea lor
+			if_ndef_found = ifndef_found(token, pereche_macro_valoare, *nr_total);
 		}
 
 		token = strtok(NULL, s);
 	}
 }
 
+void scriere_stdout_pt_reasamblare(char* buf)
+{
+	fseek(stdout, 0, SEEK_END);
+	fputs(buf, stdout);
+	fprintf(stdout, "\n");
+}
+
+void scriere_in_fisier_pt_reasamblare(char* buf, char* outfile)
+{
+	FILE* f = fopen(outfile, "a");
+	fseek(f, 0, SEEK_END);
+	fputs(buf, f);
+
+	fprintf(f, "\n");
+	fclose(f);
+}
+
+void reasamblare(char* sir, char* s, char* outfile, char** pereche_macro_valoare, int* nr_perechi)
+{
+	char* token;
+	char buf[max_size];
+	int gasit = 0;
+	
+	/* get the first token */
+	token = strtok(sir, s);
+
+	/* walk through other tokens */
+	while (token != NULL)
+	{
+		for (int i = 0; i < *nr_perechi; i+=2)
+		{
+			if (strcmp(token, pereche_macro_valoare[i]) == 0)
+			{
+				strcpy(buf, pereche_macro_valoare[i + 1]);
+				gasit = 1;
+			}
+		}
+
+		if (gasit == 1) strcpy(buf, " ");
+
+		strcpy(buf, token);
+
+		token = strtok(NULL, s);
+	}
+
+	if (strcmp(outfile, no_value) == 0) scriere_stdout_pt_reasamblare(buf);
+	else scriere_in_fisier_pt_reasamblare(buf, outfile);
+}
+
+void dezlipire(char* sir, char* directory_name, char* infile, char* outfile, char** pereche_macro_valoare, int* nr_perechi, int* nr_total)
+{
+	// posibil sa am directive de define sau include in structurile conditionale, asa ca pt ele resetez flag-urile pt include si define
+	const char s[] = " \t\n";
+	char* token;
+	*nr_perechi = 1;
+	*nr_total = *nr_perechi * 2;
+	int if_def_found, if_ndef_found, if_elif, any_type_if_found = 0;
+
+
+	/* get the first token */
+	token = strtok(sir, s);
+	if (token[0] == '#')
+		initializare_elemente_necesare_rularii(token, (char*)s, directory_name, outfile, pereche_macro_valoare, nr_perechi, nr_total);
+	else reasamblare(sir, (char*)s, outfile, pereche_macro_valoare, nr_perechi);
+	
+}
+
 void executing(char* denumire_macro, char* valoare_macro, char* directory_name, char* nume_infile, char* nume_outfile)
 {
 	int define_found = 0, if_found = 0, elif_found = 0, else_found = 0, ifdef_found = 0, ifndef_found = 0, endif_found = 0;
 	char buf[max_size];
+	char* pereche_macro_valoare = NULL;
+	int nr_perechi, nr_total;
 
 	if (strcmp(nume_infile, no_value)) // citesc de la stdin
 	{
 		if (strcmp(nume_outfile, no_value) == 0) // afisez la stdout
 		{
 			fgets(buf, max_size, stdin);
-			dezlipire(buf, directory_name, nume_infile, nume_outfile);
+			dezlipire(buf, directory_name, nume_infile, nume_outfile, &pereche_macro_valoare, &nr_perechi, &nr_total);
 		}
 	}
 }
@@ -584,27 +714,23 @@ void executing(char* denumire_macro, char* valoare_macro, char* directory_name, 
 /* so-cpp -D macro[=valoare] -I dir infile -o outfile*/
 // testul 6 -> -o nu apare
 
-void program_principal(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
 	char* denumire_macro = NULL, * valoare_macro = NULL, * directory_name = NULL, * nume_infile = NULL, * nume_outfile = NULL;
 	int x = 1; // ca sa tin evidenta indexului lui argv
 	int total_argumente_pt_argv = argc - 1; // argv[1] - argv[argc-1]
 	int continua = 0;
+	char* pereche_macro_valoare = NULL;
 	/* argc = 8 maxim
-	   argc = 4 minim*/
-	   // argv[0] e numele programului
+	   argc = 4 minim
+	*/
+	// argv[0] e numele programului
 
 	if (argc == 1) fprintf(stdout, "No arguments. Exiting....");
 	else
 	{
-		get_all_details(argv, x, continua, denumire_macro, valoare_macro, directory_name, nume_infile, nume_outfile, total_argumente_pt_argv);
+		get_all_details(argv, x, continua, &pereche_macro_valoare, directory_name, nume_infile, nume_outfile, total_argumente_pt_argv);
 
 		executing(denumire_macro, valoare_macro, directory_name, nume_infile, nume_outfile);
 	}
-}
-
-int main(int argc, char* argv[])
-{
-	//program_principal(argc, argv);
-	testare();
 }
